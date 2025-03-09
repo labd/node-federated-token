c234a682204be1a1c719df389cbcc0e2385fd7e3
Michael van Tellingen <m.vantellingen@labdigital.nl>
2025-03-09 18:08:33 +0100

    fix(apollo): return GraphQLError if token invalid
    We can only return `GraphQLError` in the `didResolveOperation` handler,
    so return a listener with exactly that

--- packages/apollo/src/gateway.test.ts
+++ packages/apollo/src/gateway.test.ts
@@ -35,8 +35,12 @@ describe("GatewayAuthPlugin", async () => {
 
 	const typeDefs = `#graphql
 	type Query {
+		hello(name: String): String!
 		testToken(create: Boolean, value: String): String!
 		refreshToken: String!
+	}
+	type Mutation {
+		createToken(create: Boolean, value: String, expired: Boolean): String!
 	}
 	`;
 	const resolvers = {
@@ -52,7 +56,7 @@ describe("GatewayAuthPlugin", async () => {
 				if (create) {
 					context.federatedToken.setAccessToken("foo", {
 						token: "bar",
-						exp: Date.now() + 1000,
+						exp: Math.floor(Date.now() / 1000 + 1000),
 						sub: "my-user-id",
 					});
 					context.federatedToken.setRefreshToken("foo", "bar");
@@ -64,12 +68,50 @@ describe("GatewayAuthPlugin", async () => {
 
 				return JSON.stringify(context.federatedToken);
 			},
+			hello: (
+				_: unknown,
+				{ name }: { name: string },
+				context: PublicFederatedTokenContext,
+			) => {
+				return `Hello ${name}`;
+			},
 			refreshToken: (_: unknown, context: PublicFederatedTokenContext) => {
 				context.federatedToken?.setAccessToken("foo", {
 					token: "bar",
-					exp: Date.now() + 1000,
+					exp: Math.floor(Date.now() / 1000 - 1000),
 					sub: "my-user-id",
 				});
+				return JSON.stringify(context.federatedToken);
+			},
+		},
+		Mutation: {
+			createToken: (
+				_: unknown,
+				{
+					create,
+					value,
+					expired,
+				}: { create: boolean; value: string; expired: boolean },
+				context: PublicFederatedTokenContext,
+			) => {
+				if (!context.federatedToken) {
+					throw new Error("No federated token");
+				}
+				if (create) {
+					context.federatedToken.setAccessToken("foo", {
+						token: "bar",
+						exp: Math.floor(
+							expired ? Date.now() / 1000 - 1000 : Date.now() / 1000 + 1000,
+						),
+						sub: "my-user-id",
+					});
+					context.federatedToken.setRefreshToken("foo", "bar");
+				}
+
+				if (value) {
+					context.federatedToken.setValue("value", value);
+				}
+
 				return JSON.stringify(context.federatedToken);
 			},
 		},
@@ -221,5 +263,70 @@ describe("GatewayAuthPlugin", async () => {
 
 		assert.isNotEmpty(newAccessToken);
 		assert.notEqual(newAccessToken, accessToken);
+	});
+
+	it("should return GraphQLError when token expired", async () => {
+		const context = {
+			federatedToken: new PublicFederatedToken(),
+			res: httpMocks.createResponse(),
+			req: httpMocks.createRequest(),
+		};
+		await testServer.executeOperation(
+			{
+				query:
+					"mutation createToken { createToken(create: true, expired: true) }",
+				http: {
+					headers: new HeaderMap(),
+					method: "POST",
+					search: "",
+					body: "",
+				},
+			},
+			{
+				contextValue: context,
+			},
+		);
+		expect(context.res.statusCode).toBe(200);
+		expect(context.res.get("x-access-token")).toBeDefined();
+		expect(context.res.get("x-refresh-token")).toBeDefined();
+		const accessToken = context.res.get("x-access-token");
+
+		// Set received token
+		const newContext = {
+			federatedToken: new PublicFederatedToken(),
+			res: httpMocks.createResponse(),
+			req: httpMocks.createRequest({
+				headers: {
+					"x-access-token": `Bearer ${accessToken}`,
+				},
+			}),
+		};
+
+		const response = await testServer.executeOperation(
+			{
+				query: 'query hello { hello(name: "foobar") }',
+				http: {
+					headers: new HeaderMap([["x-access-token", `Bearer ${accessToken}`]]),
+					method: "POST",
+					search: "",
+					body: "",
+				},
+			},
+			{
+				contextValue: newContext,
+			},
+		);
+
+		expect(response.http.status).toBe(401);
+		assert(response.body.kind === "single"); // Make typescript happy
+		const errors = response.body.singleResult.errors;
+		expect(errors).toStrictEqual([
+			{
+				extensions: {
+					code: "UNAUTHENTICATED",
+				},
+				message: "Your token has expired.",
+			},
+		]);
 	});
 });
