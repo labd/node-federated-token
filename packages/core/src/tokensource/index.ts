2ff492b8c5d01b812d95d03c50868da8cd05bfc5
Michael van Tellingen <m.vantellingen@labdigital.nl>
2025-05-20 06:58:49 +0200

    chore!: Remove `CookieTokenSource` from core
    The `CookieTokenSource` is available within the
    `@labdigital/federated-token-express-adapter` package as
    `ExpressCookieTokenSource`

--- packages/core/src/tokensource/index.ts
+++ packages/core/src/tokensource/index.ts
@@ -1,6 +1,5 @@
 export type { TokenSource } from "./base";
 export { CompositeTokenSource } from "./composite";
-export { CookieTokenSource } from "./cookies";
 export { HeaderTokenSource } from "./headers";
 export {
 	BaseCookieTokenSource,
