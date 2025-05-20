8b7dc5f05ed4a1d9e3d083a6c1af669e415418ca
Michael van Tellingen <m.vantellingen@labdigital.nl>
2025-05-20 11:06:46 +0200

    chore(core): improve masking of tokens

--- packages/apollo/src/utils.ts
+++ packages/apollo/src/utils.ts
@@ -1,5 +1,5 @@
-export function maskToken(token: string, visibleChars = 3): string {
+export function maskToken(token: string, visibleChars = 3, maxLenght = 10): string {
 	if (!token) return "(missing!)";
-	if (token.length <= visibleChars) return "*".repeat(token.length);
-	return token.slice(0, visibleChars) + "*".repeat(token.length - visibleChars);
+	if (token.length <= visibleChars) return "*".repeat(maxLenght - token.length);
+	return token.slice(0, visibleChars) + "*".repeat(maxLenght - visibleChars);
 }
