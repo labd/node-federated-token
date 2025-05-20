ea909eceab156681e10ae0d54f5fad40f1fe442b
Michael van Tellingen <m.vantellingen@labdigital.nl>
2025-05-20 11:15:13 +0200

    style: resolve formatting issues

--- packages/apollo/src/utils.ts
+++ packages/apollo/src/utils.ts
@@ -1,4 +1,8 @@
-export function maskToken(token: string, visibleChars = 3, maxLenght = 10): string {
+export function maskToken(
+	token: string,
+	visibleChars = 3,
+	maxLenght = 10,
+): string {
 	if (!token) return "(missing!)";
 	if (token.length <= visibleChars) return "*".repeat(maxLenght - token.length);
 	return token.slice(0, visibleChars) + "*".repeat(maxLenght - visibleChars);
